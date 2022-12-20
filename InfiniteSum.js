1b921c29e5633be705ae86b54855a9d27d08699d
ahmedashfaq850 <ahmedashfaq6777@gmail.com>
2022-12-20 20:48:59 +0500

    updated

---
+++ InfiniteSum.js
@@ -0,0 +1,11 @@
+// this question is solved using currying 
+
+function addInfinite(a){
+    return function (b){
+        if (b) return addInfinite(a+b);
+        return a;
+    }
+}
+
+const res = addInfinite(2)(2)(3)(4)()
+console.log(res)
